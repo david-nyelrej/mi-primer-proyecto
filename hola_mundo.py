54b880db97695ff85638f60fc85e7d21bd77224e
David G <david.nyelrej@gmail.com>
2026-03-08 02:08:06 -0500

    mi primer commit: agregando calculadora hola mundo y leame

---
+++ hola_mundo.py
@@ -0,0 +1,8 @@
+# Programa: Hola Mundo 
+# Autor: Tu Nombre 
+# Fecha: Diciembre 2025 
+def saludar():
+    print("¡Hola, mundo desde Git!") 
+    print("Este es mi primer proyecto versionado") 
+    
+    if__name__== "__ main__": saludar()
